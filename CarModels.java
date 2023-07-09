1fcedb66e033ee398eb89847c062d2959e88ad38
LAKSHMI PRASANNA TANGUTURI <88602508+prasanna61@users.noreply.github.com>
2023-07-09 22:22:57 +0530

    question 1

--- CarModels.java
+++ CarModels.java
@@ -1,3 +1,4 @@
+package Mainproject;
 public class CarModels {
     public static void main(String[] args) {
         String [] carmodel= {"Polo Trendline","Polo Highline","Virtus Trendline","Virtus Highline","Taigun Trendline","Taigun Highline","Taigun Topline"};
