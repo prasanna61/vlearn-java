1f7a48cefabbbcd7bac30faaa0be8b9b91fecb21
LAKSHMI PRASANNA TANGUTURI <88602508+prasanna61@users.noreply.github.com>
2023-07-09 22:00:59 +0530

    Question 1

---
+++ CarModels.java
@@ -0,0 +1,10 @@
+public class CarModels {
+    public static void main(String[] args) {
+        String [] carmodel= {"Polo Trendline","Polo Highline","Virtus Trendline","Virtus Highline","Taigun Trendline","Taigun Highline","Taigun Topline"};
+		System.out.println("carmodels are");
+        for(int i=0;i<carmodel.length;i++)
+		{
+			System.out.println(carmodel[i]);
+        }	
+    }
+}
